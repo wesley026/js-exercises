d36a0568e9e39eae70d1a6cd4091120fffb92e9f
wesley026 <127734144+wesley026@users.noreply.github.com>
2023-04-05 18:30:20 -0700

    finished using for-of statement to loop through, using .include function to check if the item is in the inputted arguements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+    // arbitary number of arguements
+    let arr = [];
+    // intialize new empty array
+    for (item of array)
+    {
+        if (!args.includes(item)){
+            arr.push(item);
+        }
+    }
 
+    // loop through each element in array and if the arguements don't include the 
+    // item that is being looped through 
+    // add that item into the empty array 
+
+    // using for-of to loop through each object in the array instead of the weird forEach function 
+    // for-in to refer to index
+
+    return arr ;
+
+    
 };
 
 // Do not edit below this line
