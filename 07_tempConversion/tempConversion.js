59eba7cd04f5a1d668f0ad66e2492005858df52b
wesley026 <127734144+wesley026@users.noreply.github.com>
2023-04-06 13:28:51 -0700

    something wrong with the tests same output but failed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let temp = (x- 32)*(5/9);
+  if (temp!= 0){
+    temp = temp.toFixed(1);
+  }
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  
+  let temp = (5/9)*x + 32;
+  if (temp != 0){
+    temp = temp.toFixed(1);
+  }
+  return temp;
 };
 
 // Do not edit below this line
