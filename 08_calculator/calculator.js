6fc0e3d90a3600659501ed2f05267c62d13d132e
wesley026 <127734144+wesley026@users.noreply.github.com>
2023-04-06 16:33:11 -0700

    works but again the tests are weird

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return  a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let count = 0;
+  for (let num of array){
+    count += num;
+  }
+
+  return count;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let prod = 1;
+  for (let num of array){
+    prod *= num;
+  }
 
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let prod = 1;
+  if (a== 0){
+    return prod;
+  }
+  
+  for (let i= a; i<= 1; i--){
+    prod *= i;
+  }
+  return prod;
 };
 
 // Do not edit below this line
