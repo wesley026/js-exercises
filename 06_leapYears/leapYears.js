2c7de2caa251ab338112b741ddf3fc349a7dfec3
wesley026 <127734144+wesley026@users.noreply.github.com>
2023-04-06 04:37:19 -0700

    finished leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(a) {
+    if (a % 400 == 0){
+        return true;
+    }
+    if (a % 100 == 0){
+        return false;
+    }
 
+    if (a % 4 == 0){
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
