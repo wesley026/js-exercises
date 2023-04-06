93a2f9c130c5d6c071f35d0555b7f56b3084e6c6
wesley026 <127734144+wesley026@users.noreply.github.com>
2023-04-05 19:31:59 -0700

    finished sum_All

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
 
+
+const sumAll = function(a,b) {
+    let sum = 0;
+    if (a < 0 || b < 0){
+        return "ERROR";
+    }
+
+    if (typeof(a)!= "number" || typeof(b)!= "number"){
+        return "ERROR";
+    }
+    for (let i = Math.min(a,b) ; i <= Math.max(a,b) ; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
