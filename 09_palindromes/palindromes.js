863a14fe1773626f3c41dde0d2b27117bf24bad0
wesley026 <127734144+wesley026@users.noreply.github.com>
2023-04-10 14:12:46 -0700

    finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // need to filte non-aplphanumetic characters
 
+    // matches all non-alphanumetic characters and underscore by searching globally 
+    // turns the inputted str to all lower case  and replace all the matched non-alphanumeric characters 
+    // stored in reg with  empty char/ string
+    let reg = /[\W_]/g;
+    let smallStr = str.toLowerCase().replace(reg,"");
+    
+    let  reversed  = smallStr.split("").reverse().join("");
+    if (reversed == smallStr) return true;
+    
+        
+        return false;
+    
 };
 
 // Do not edit below this line
