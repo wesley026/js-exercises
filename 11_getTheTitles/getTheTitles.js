6807ab3bbda1e82b94a477d12470e047bfec5385
wesley026 <127734144+wesley026@users.noreply.github.com>
2023-04-10 15:10:28 -0700

    using push to get value of dictionaries stored in arrays

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let titles = [];
+    for (let i = 0; i < array.length; i++) {
+      titles.push(array[i].title);
+    }
+    // array[i].title refers to the value of "title" of
+    // the ith idex in array 
+    return titles;
 };
 
 // Do not edit below this line
