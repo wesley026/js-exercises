83e342486e6d2016af6d0b7b7625467382519216
wesley026 <127734144+wesley026@users.noreply.github.com>
2023-04-10 14:42:03 -0700

    fibonacci recursion

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a< 0) return "OOPS";
+    if (a == 1 || a == 2) return 1;
+    return fibonacci(a-1)+ fibonacci(a-2);
 };
 
 // Do not edit below this line
